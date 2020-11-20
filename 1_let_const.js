300f2ae8f6c8e4c7beea4456347f230561fe5c9c
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-20 16:42:22 +0200

    Discovered about diff types of variables let&const

--- 1_let_const.js
+++ 1_let_const.js
@@ -1,4 +1,4 @@
-let a = 24
+/* let a = 24
 
 if(true) {
     let a = 42
@@ -6,5 +6,44 @@ if(true) {
     console.log('a', a)
 }
 
-console.log(a)
+console.log(a) */
 
+
+// Hosting
+/* b = 20
+
+console.log(b)
+
+var b = 10 */
+
+/* function hoisted() {
+    age = 19
+}
+
+let age
+hoisted() 
+console.log(age) */
+
+// Const
+
+/* const COLOR = '#000'
+//COLOR = '#FFF'
+
+console.log(COLOR)
+
+const array = [1, 2, 3, 5, 8]
+
+//array = 23
+
+console.log(array)
+array.push(13)
+console.log(array)
+
+const obj = {
+    a: 42
+}
+
+obj.name = 'Vitalii'
+obj.age = 19
+
+console.log(obj) */
