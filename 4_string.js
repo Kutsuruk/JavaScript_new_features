c6905a91523fa8a3023983c7f37b5274e0d82125
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-21 18:12:28 +0200

    String methods

--- 4_string.js
+++ 4_string.js
@@ -7,6 +7,23 @@ ${isVisible() ? `<p>Visible</p>` : `<p>Non visible</p>` }
 <h1 id="demo" style="color: red">${title}</h1>
 `
 
-console.log(template) */
+console.log(template) */ 
 
 // Methods
+/* const str = 'Hello' 
+
+console.log(str.startsWith('H'))
+console.log(str.startsWith('e'))
+console.log(str.endsWith('o'))
+console.log(str.endsWith('k'))
+
+console.log(str.includes('ello'))
+
+console.log(str.repeat(3))
+
+console.log(str.trim())
+console.log(str.trimEnd())
+console.log(str.trimStart())
+
+console.log(str.padStart(10, '-'))
+console.log(str.padEnd(10, '-')) */
