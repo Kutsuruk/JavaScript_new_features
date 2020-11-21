3ca47138c549c37bf137e74081ed63af77eac140
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-22 00:01:29 +0200

    Get and methods

---
+++ 12_map_set.js
@@ -0,0 +1,23 @@
+const map = new Map(
+    [ ['a', 1] ]
+)
+
+// console.log(map.get('a'))
+map.set('b', 2).set('c', 3).set('NaN', NaN)
+
+/*
+console.log(map)
+console.log(map.get('NaN'))
+// map.clear()
+console.log(map.has('NaN'))
+console.log(map.has('42'))
+// console.log(map.delete('b'))
+console.log(map.size) 
+*/
+
+/* 
+console.log(map.keys())
+console.log(map.values())
+console.log(map.entries()) 
+*/
+
