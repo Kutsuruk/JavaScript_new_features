b0a6fba8c47d1aca1889ef76aded603c9a4769d6
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-21 19:05:20 +0200

    Destructuring in arr/obj, rest/spread in arr/obj

--- 5_rest_spread.js
+++ 5_rest_spread.js
@@ -24,3 +24,30 @@ const array  = [1, undefined, 3, 5, 8, 13]
 // const [a, , c] = array
 // console.log(a, c)
 
+// Object
+
+const address = {
+    country: 'USA',
+    city: 'New-York',
+    street: 'Sanderles',
+    concat() {
+        return `${this.country}, ${this.city}, ${this.street}`
+    }
+}
+
+// const {country = 'Sweden', city = 'Stockholm', street = 'Kontilantie', concat: addressConcat} = address
+
+// console.log(country, city, street)
+// console.log(addressConcat.call(address))
+
+/* const {city, ...rest} = address
+console.log(city)
+console.log(rest)
+
+const newAddress = {...address, country: 'Finland', city: 'Helsinki', street: 'Karhuapio', code: 368} // copy of object address with spread operator
+
+console.log(newAddress)
+console.log(newAddress.concat()) */
+
+// console.log(address.concat())
+
