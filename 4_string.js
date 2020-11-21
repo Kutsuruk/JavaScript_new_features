70e631df36f61140720231d4298f3e335473738b
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-21 18:00:39 +0200

    Learnt about different types of string

---
+++ 4_string.js
@@ -0,0 +1,12 @@
+/* const title = 'Hello'
+const isVisible = () => Math.random() > 0.5
+
+
+const template = `
+${isVisible() ? `<p>Visible</p>` : `<p>Non visible</p>` }
+<h1 id="demo" style="color: red">${title}</h1>
+`
+
+console.log(template) */
+
+// Methods
