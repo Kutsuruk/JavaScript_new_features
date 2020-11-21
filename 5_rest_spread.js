62f4871cb7748e2bcb8103df1367ec5f4e79e46c
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-21 18:48:12 +0200

    Rest and Spread operators

---
+++ 5_rest_spread.js
@@ -0,0 +1,15 @@
+// Rest
+function average(...args) {
+    return args.reduce((acc, i) => acc += i, 0) / args.length
+}
+//console.log(average([10, 20, 30, 40]))
+
+// Spread
+// const array  = [1, 2, 3, 5, 8, 13]
+// console.log(...array)
+// console.log(Math.max(...array)) // find highest value of array
+// console.log(Math.min(...array)) // find lowest value of array
+
+// const fib = [1, ...array]
+// console.log(fib)
+
