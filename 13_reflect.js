91992f9ef66782236793cbd982d30a158b81b35f
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-22 00:15:34 +0200

    Reflect`s methods

--- 13_reflect.js
+++ 13_reflect.js
@@ -20,3 +20,12 @@ console.log(student)
 // console.log(student.__proto__ === ProtoStudent.prototype)
 
 // Methods
+
+Reflect.apply(student.greet, {name: 'Anastasia'}, [])
+console.log(Reflect.ownKeys(student))
+
+// Reflect.preventExtensions(student) // block changes of this object
+console.log(Reflect.isExtensible(student)) // checking is object blocked for changeble
+student.age = 25
+
+console.log(student)
